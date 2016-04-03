eb3abbf3fe2483cdc8086264e98107a2f7187336
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-04-03 11:15:58 +0200

    Add setting up smtp server

--- personalized_emails.py
+++ personalized_emails.py
@@ -3,14 +3,20 @@ A small python toolkit that creates and sends personalized
 emails from `jinja2` templates and a pandas-readable database
 
 Usage:
-    personalized-emails <template> <database>
+    personalized-emails <template> <database> [options]
+
+Options:
+    -c <config>, --config=<config>  config-file [default: config.cfg]
 '''
-import jinja2
+
 import pandas as pd
-from tqdm import tqdm
-from docopt import docopt
+import jinja2
 import frontmatter
 import gfm
+import smtplib
+from docopt import docopt
+from configparser import RawConfigParser
+from getpass import getpass
 
 
 def read_database(inputfile, **kwargs):
@@ -40,9 +46,23 @@ def parse_template(inputfile):
     return template, result.metadata
 
 
+def setup_smtp_server(host, port, user, password=None):
+    smtp = smtplib.SMTP_SSL(host, port)
+    if password is None:
+        password = getpass('Please enter your mail password: ')
+    smtp.login(user, password)
+
+
 def main():
     args = docopt(__doc__, version='PeP et Al. emails v0.0.1')
     database = read_database(args['<database>'])
+
+    config = RawConfigParser()
+    successful_files = config.read(args['--config'])
+    if not successful_files:
+        raise IOError('Could not read config-file')
+
+    mail_server = setup_smtp_server(**config['smtp'])
 
     template, metadata = parse_template(args['<template>'])
 
@@ -50,10 +70,6 @@ def main():
         content = template.render(recipient=recipient)
         html = gfm.markdown(content)
 
-        with open('test_{}.html'.format(recipient.firstname), 'w') as f:
-            f.write(html)
-
 
 if __name__ == '__main__':
     main()
-
